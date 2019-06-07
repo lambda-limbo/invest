446c143008a28cfb7f62ad93cac14b1a6476b5b8
Rafael Campos Nunes <rafaelnunes@engineer.com>
2019-06-07 14:13:21 -0300

    Add bare pages to be filled by other members
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -9,9 +9,11 @@
 
     // Load the templates folder
     $loader = new \Twig\Loader\FilesystemLoader('../app/_layouts');
+
     // Load the environment and cache the results 
     $twig = new \Twig\Environment($loader, [
-        'cache' => '../app/_cache'
+        // Turn off the cache when in development, in the future we have to find a new way to do this.
+        //'cache' => '../app/_cache'
     ]);
 
     
@@ -19,7 +21,28 @@
 
     // Define all the routes of the system
     $router->get('/', function() use($twig) {
-        echo $twig->render('default.twig', ['navigation' => array('HOME', 'CONTACT', 'LOGIN', 'SIGN UP')]);
+        echo $twig->render('default.twig', ['navigation' => array('quem somos' => '/about', 'investimentos' => 'investments', 'contatos' => 'contact', 'login' => 'login', 
+                                            'abra sua conta' => '/signup', '' => '/')]);
     });
+
+    $router->get('/login', function() use($twig) {
+       echo $twig->render('login.twig', ['navigation' => array('quem somos' => '/about', 'investimentos' => '/investments', 'contatos' => '/contact', 'login' => 'login', 
+                                         'abra sua conta' => '/signup')]);
+    });
+
+    $router->post('/login', function() use($twig) {
+    });
+
+    $router->get('/about', function() use($twig) {
+        echo $twig->render('about.twig', ['navigation' => array('quem somos' => '/about', 'investimentos' => '/investments', 'contatos' => '/contact', 'login' => 'login', 
+                                          'abra sua conta' => '/signup')]);
+    });
+
+    $router->set404(function()  use($twig) {
+        header('HTTP/1.1 404 Not Found');
+        // ... do something special here
+        echo $twig->render('404.twig');
+    });
+
 
 
