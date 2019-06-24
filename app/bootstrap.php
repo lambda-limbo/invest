3daf43045c30c9e20e9f04ea6ac296e29097bb60
Rafael Campos Nunes <rafaelnunes@engineer.com>
2019-06-24 11:40:01 -0300

    Add a namespace for the application
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,8 +1,5 @@
 <?php
-    /**
-     * The initial file that serves the client with pages of the system.
-     */
-        
+    namespace Invest;
 
     require_once "../vendor/autoload.php";
 
@@ -15,56 +12,6 @@
         //'cache' => '../app/_cache'
     ]);
 
-    
     $router = new \Bramus\Router\Router();
 
-    // Global routing variable
-    $routes = array('quem somos' => '/about', 
-                    'investimentos' => '/investments', 
-                    'contatos' => '/contact', 
-                    'login' => '/login',  
-                    'abra sua conta' => '/sign_up');
-
-
-    $twig->addGlobal('navigation', $routes);
-
-    // Define all the routes of the system
-    $router->get('/', function() use($twig) {
-        echo $twig->render('home.twig');
-    });
-
-    $router->get('/login', function() use($twig) {
-       echo $twig->render('login.twig');
-    });
-
-    $router->post('/login', function() use($twig) {
-    });
-
-    $router->get('/about', function() use($twig) {
-        echo $twig->render('about.twig');
-    });
-
-    $router->get('/sign_up', function() use($twig) {
-        echo $twig->render('sign_up.twig');
-    });
-
-    $router->post('/sign_up', function() use($twig) {
-        
-    });
-
-    $router->get('/investments', function() use($twig) {
-        echo $twig->render('investments.twig');
-    });
-
-    $router->get('/investments', function() use($twig) {
-        echo $twig->render('contact.twig');
-    });
-
-    $router->set404(function()  use($twig) {
-        header('HTTP/1.1 404 Not Found');
-        // ... do something special here
-        echo $twig->render('404.twig');
-    });
-
-
-
+    require_once 'routing.php';
