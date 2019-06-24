55918e27c8941e0144bc366355c340af750bfffa
Rafael Campos Nunes <rafaelnunes@engineer.com>
2019-06-24 16:31:58 -0300

    Fix database inconsistencies
        Some inconsistencies are removed in this commit. Some changes are
    also made in order to simplify the database.
    
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- database/insert.sql
+++ database/insert.sql
@@ -4,7 +4,9 @@
 
 USE INVEST;
 
-CALL P_INSERT_ADM('Rafael Campos Nunes', 0, 'ranu', 'ranu@invest.com', '0000-00-00 00:00:00',
-                  '999211031');
-CALL P_INSERT_ADM('Luiz Otavio', 0, 'luizotaviogb', 'luiz@invest.com', '0000-00-00 00:00:00',
-                   '999446250');
+-- The password values are examples because actually the password will be hashed.
+
+CALL P_INSERT_ADM('Rafael Campos Nunes', 'ranu', '1nv35t@admin@ccoun1', '02086936290', 'ranu@invest.com', 
+                  '999211031', '0000-00-00 00:00:00', 0.0);
+CALL P_INSERT_ADM('Luiz Otavio Goebrey', 'ranu', '1nv35t@admin@ccoun2', '02654362950', 'luiz@invest.com', 
+                  '996214862', '0000-00-00 00:00:00', 0.0);
