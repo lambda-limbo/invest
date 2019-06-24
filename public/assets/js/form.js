b8c40a4930d769007d63087da046e1e441ccb389
Rafael Campos Nunes <rafaelnunes@engineer.com>
2019-06-24 11:34:12 -0300

    Adds a back button and modifies the placeholders
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- public/assets/js/form.js
+++ public/assets/js/form.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let follow = document.getElementById('next');
+let back = document.getElementById('back');
 
 follow.addEventListener('click', (e) => {
     e.preventDefault();
@@ -11,3 +12,13 @@ follow.addEventListener('click', (e) => {
     first.style.display = 'none';
     second.style.display = 'block';
 })
+
+back.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    let first = document.getElementById('first');
+    let second = document.getElementById('second');
+
+    first.style.display = 'block';
+    second.style.display = 'none';
+});
