42efc1c2e26b1dc02a7eee3ba58062a38ac6a389
luizotaviogb <luizotaviogb@gmail.com>
2019-07-01 11:01:04 -0300

    Arrumando erros indentação

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,14 +28,16 @@ class User {
 
     public function getName() {
         return $this->name;
-   	}
-   	public function setName($name) {
+    }
+
+    public function setName($name) {
         $this->name = $name;
    	}
 
     public function getWallet() {
         return $this->wallet;
    	}
+
     public function setWallet($wallet) {
         $this->wallet = $wallet;
    	}
@@ -43,7 +45,6 @@ class User {
     public function getPassword() {
         return $this->password;
    	}
-
     public function setPassword($password) {
         $this->password = $password;
    	}
@@ -58,33 +59,40 @@ class User {
    	public function getCPF() {
         return $this->cpf;
    	}
+    
    	public function setCPF($cpf) {
         $this->cpf = $cpf;
+    }
 
     public function getEmail() {
 	   return $this->email;
-   	}
-   	public function setEmail($email) {
+    }
+
+    public function setEmail($email) {
         $this->email = $email;
     }
+
     public function getDate_birth() {
 	   return $this->email;
    	}
-   	public function setDate_birth($date) {
+
+    public function setDate_birth($date) {
         $this->date_birth = $date;
     }
 
     public function getPhone() {
 	   return $this->phone;
     }
-   	public function setPhone($phone) {
+
+    public function setPhone($phone) {
         $this->phone = $phone;
     }
    	
-   	public function getAdmin() {
+    public function getAdmin() {
 	   return $this->admin;
    	}
-   	public function setAdmin($admin) {
+
+    public function setAdmin($admin) {
         $this->admin = $admin;
     }
 }
