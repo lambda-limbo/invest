61c18641151fa7759cbe9f1ace628234b206e3c3
Rafael Campos Nunes <rafaelnunes@engineer.com>
2019-07-09 14:18:30 -0300

    Fix indentation and removes the DAO
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- app/Middleware/Authentication.php
+++ app/Middleware/Authentication.php
@@ -1,30 +1,33 @@
 <?php declare(strict_types=1);
 
-    namespace Invest\Middleware;
+namespace Invest\Middleware;
 
-    use Invest\Database\Connection;
+use Invest\Database\Connection;
 
-    /**
-     * @brief Middleware to authenticate in and out an user in the system.
-     */
-    final class Authentication {
-        private function __construct() {}
-
-        public static function authenticate(string $user, string $password) : bool {
-            if (strlen($user) == 0 || strlen($password) == 0) {
-                return false;
-            }
-
-            
-
-            $valid_username = 1;
-            $valid_password = 1;//password_verify($password, );
-
-            if ($valid_username && $valid_password) {
-                Session::create("USER", array('username' => $user));
-                return true;
-            }
-
+/**
+* Middleware to authenticate in an user in the system. The only function within
+* this class is an authentication that returns a boolean to whether or not a user
+* can enter the system.
+*/
+final class Authentication {
+    private function __construct() {
+        // does nothing    
+    }
+    
+    public static function authenticate(string $user, string $password) : bool {
+        if (strlen($user) == 0 || strlen($password) == 0) {
             return false;
         }
+        
+        
+        $valid_username = 1;
+        $valid_password = 1;//password_verify($password, );
+        
+        if ($valid_username && $valid_password) {
+            Session::create("USER", array('username' => $user));
+            return true;
+        }
+        
+        return false;
     }
+}
