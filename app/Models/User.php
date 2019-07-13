aaca4763ea46c0b8fb54b5406d8c09b2b300cc23
Rafael Campos Nunes <rafaelnunes@engineer.com>
2019-07-13 16:41:03 -0300

    Implements the User model
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,99 +1,122 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Invest\Models;
 
-class User {
-    private $name;
-    private $wallet;
-    private $password;
-    private $login;
-    private $cpf;
-    private $email;
-    private $date_birth;
-    private $phone;
-    private $admin;
+use Invest\Database\Query;
 
-
-    public function __construct($name, $wallet, $password, $login, $cpf, $email, $date_birth, $phone, $admin) {
-        $this->name = $name;
-        $this->wallet = $wallet;
-        $this->password = $password;
+class User extends Entity {
+    private $pk;
+    public $name;
+    public $wallet;
+    public $password;
+    public $login;
+    public $cpf;
+    public $email;
+    public $birth;
+    public $phone;
+    public $admin;
+    
+    
+    public function __construct($login, $password = "",  $name = "", $cpf = "", $email = "", $birth = "", $phone = "", $wallet = "", $admin = "") {
         $this->login = $login;
+        $this->password = password_hash($password, PASSWORD_DEFAULT);
+        $this->name = $name;
         $this->cpf = $cpf;
         $this->email = $email;
-        $this->date_birth = $date;
+        $this->birth = $birth;
         $this->phone = $phone;
-        $this->admin = $admin;
-    }
-
-    public function getName() {
-        return $this->name;
-    }
-
-    public function setName($name) {
-        $this->name = $name;
-    }
-
-    public function getWallet() {
-        return $this->wallet;
-    }
-
-    public function setWallet($wallet) {
         $this->wallet = $wallet;
-    }
-
-    public function getPassword() {
-        return $this->password;
-    }
-    public function setPassword($password) {
-        $this->password = $password;
-    }
-
-    public function getLogin() {
-        return $this->login;
-    }
-
-    public function setLogin($login) {
-        $this->login = $login;
-    }
-
-    public function getCPF() {
-        return $this->cpf;
-    }
-
-    public function setCPF($cpf) {
-        $this->cpf = $cpf;
-    }
-
-    public function getEmail() {
-        return $this->email;
-    }
-
-    public function setEmail($email) {
-        $this->email = $email;
-    }
-
-    public function getDate_birth() {
-        return $this->email;
-    }
-
-    public function setDate_birth($date) {
-        $this->date_birth = $date;
-    }
-
-    public function getPhone() {
-        return $this->phone;
-    }
-
-    public function setPhone($phone) {
-        $this->phone = $phone;
-    }
-   	
-    public function getAdmin() {
-	   return $this->admin;
-    }
-
-    public function setAdmin($admin) {
         $this->admin = $admin;
+    }
+    
+    public function get() : bool {
+        $q = new Query('SELECT * FROM TB_USER WHERE USER_LOGIN = :LOGIN');
+        $r = $q->execute(array(':LOGIN' => $this->login));
+
+        if ($r) {
+            $data = $q->fetch();
+
+            $this->pk = $data['USER_PK'];
+            $this->password = $data['USER_PASSWORD'];
+            $this->name = $data['USER_NAME'];
+            $this->cpf = $data['USER_CPF'];
+            $this->email = $data['USER_EMAIL'];
+            $this->birth = $data['USER_BIRTH'];
+            $this->phone = $data['USER_PHONE'];
+            $this->wallet = $data['USER_WALLET'];
+            $this->admin = $data['USER_ADM'];
+        }
+
+        return $r;
+    }
+
+    public function save() : bool {
+        $type = "";
+        
+        if ($this->admin) {
+            $type = "ADMIN";
+        } else {
+            $type = "USER";
+        }
+        
+        $query = 'CALL P_INSERT_'. $type . '(:NAME, :LOGIN, :PASSWORD, :CPF, :EMAIL, :PHONE, :BIRTH, :WALLET)';
+        
+        $q = new Query($query);
+        $r = $q->execute(array(':NAME' => $this->name, 
+                               ':LOGIN' => $this->login, 
+                               ':PASSWORD' => $this->password, 
+                               ':CPF' => $this->cpf, 
+                               ':EMAIL' => $this->email, 
+                               ':PHONE' => $this->phone, 
+                               ':BIRTH' => $this->birth, 
+                               ':WALLET' => $this->wallet));
+
+        return $r;
+    }
+
+    public function update() : bool {
+        $q = new Query('UPDATE TB_USER SET USER_NAME = :NAME, 
+                                           USER_LOGIN = :LOGIN, 
+                                           USER_PASSWORD = :PASSWORD,
+                                           USER_CPF = :CPF,
+                                           USER_EMAIL = :EMAIL,
+                                           USER_PHONE = :PHONE,
+                                           USER_BIRTH = :BIRTH,
+                                           USER_WALLET = :WALLET WHERE USER_PK = :PK');
+
+        $r = $q->execute(array(':NAME' => $this->name, 
+                            ':LOGIN' => $this->login, 
+                            ':PASSWORD' => $this->password, 
+                            ':CPF' => $this->cpf, 
+                            ':EMAIL' => $this->email, 
+                            ':PHONE' => $this->phone, 
+                            ':BIRTH' => $this->birth, 
+                            ':WALLET' => $this->wallet));
+
+        return $r;
+    }
+
+    public function delete() : bool {
+        $r = 0;
+
+        if (isset($this->pk)) { 
+            $q = new Query('DELETE FROM TABLE TB_USER WHERE USER_PK = :PK');
+            $r = $q->execute(array('PK' => $this->pk));
+
+            if ($r) {
+                $this->pk = "";
+                $this->login = "";
+                $this->password = "";
+                $this->name = "";
+                $this->cpf = "";
+                $this->email = "";
+                $this->birth = "";
+                $this->phone = "";
+                $this->wallet = "";
+                $this->admin = "";
+            }
+        }
+
+        return $r;
     }
 }
