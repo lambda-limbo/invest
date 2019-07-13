61ca6b1a88ba9ce7306e5707520ac41794b602e6
Rafael Campos Nunes <rafaelnunes@engineer.com>
2019-07-13 13:44:08 -0300

    Fix the namespace error when inheriting
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- app/Exceptions/DatabaseException.php
+++ app/Exceptions/DatabaseException.php
@@ -1,14 +1,15 @@
 <?php
     namespace Invest\Exceptions;
 
-    class DatabaseException extends Exception {
+    class DatabaseException extends \Exception {
+    
         /**
          * @brief Creates a new exception for throwing it wherever you want.
          * @param message The message of the exception
          * @param code The exception code
          * 
          */
-        public function __construct($message, $code, Exception $previous = null) {
+        public function __construct($message, $code = 0, Exception $previous = null) {
             parent::__construct($message, $code, $previous);
         }
 
