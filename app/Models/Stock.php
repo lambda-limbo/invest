1ddae3b729229b0dba75f68801d95901ebdbbbe1
Larissa <larividal95@gmail.com>
2019-07-14 14:19:27 -0300

    Stock entity
    Functions of the entity class added in the stock class.

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -1,64 +1,107 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Invest\Models;
+
 use Invest\Models\Company;
-class Stock {
-	private $minimium;
-	private $maximum;
-	private $opening;
-	private $value;
-	private $close_value;
-	private $company;
+use Invest\Database\Connection;
+use Invest\Database\Query;
+
+class Stock implements Entity {
+	private $pk;
+	public $minimium;
+	public $maximum;
+	public $opening_value;
+	public $close_value;
+	public $company;
 
 
-	public function __construct($minimium, $maximum, $opening, $value, $close_value, $company) {
-		$this->minimium = $minimium;
-		$this->maximum = $maximum;
-		$this->opening = $opening;
-		$this->value = $value;
-		$this->close_value = $close_value;
-		$this->company = $company;
+	public function __construct(double $minimium = null, double $maximum = null, double $opening_value = null, double $close_value = null, int $company = null) {
+		if (isset($minimium)) {
+			$this->minimium = $minimium;
+		}
+		if (isset($maximum)) {
+			$this->maximium = $maximum;
+		}
+		if (isset($opening_value)){
+			$this->opening_value = $opening_value;
+		}
+		if (isset($close_value)){
+			$this->close_value = $close_value;
+		}
+		if (isset($company)){
+			$this->company = $company;
+		}
 	}
 	
 	public function getMinimium() {
 		return $this->minimium;
 	}
-	public function setMinimium($minimium) {
-		$this->minimium = $minimium;
+
+	public function get() : bool {
+		$q = new Query('SELECT * FROM TB_STOCK WHERE TB_COMPANY_COMPANY_PK = :COMPANY');
+		$r = $q->execute(array(' :COMPANY' => $this->company));
+
+		if ($r) {
+			$data = $q->fetch();
+
+			$this->pk = $data['STOCK_PK'];
+			$this->minimium = $data['STOCK_MINIMIUM'];
+			$this->maximium = $data['STOCK_MAXIMIUM'];
+			$this->opening_value = $data['STOCK_OPEN_VALUE'];
+			$this->close_value = $data['STOCK_CLOSE_VALUE'];
+		}
+
+		return $r;
 	}
 
-	public function getMaximum() {
-		return $this->maximum;
-	}
-	public function setMaximum($maximum) {
-		$this->maximum = $maximum;
+	public function save() : bool {
+		$query = 'CALL P_INSERT_'.'(:MINIMIUM, :MAXIMIUM, :OPENING_VALUE, :CLOSE_VALUE, :COMPANY)';
+
+		$q = new Query($query);
+
+		$r = $q->execute(array(':MINIMIUM' => $this->minimium,
+							   ':MAXIMIUM' => $this->maximium,
+							   ':OPENING_VALUE' => $this->opening_value,
+							   ':CLOSE_VALUE' => $this->close_value,
+							   ':COMPANY' => $this->company));
+
+		return $r;
 	}
 
-	public function getOpening() {
-		return $this->opening;
-	}
-	public function setOpening($opening) {
-		$this->opening = $opening;
+	public function update() : bool {
+		$q = new Query('UPDATE TB_STOCK SET STOCK_MINIMIUM = :MINIMIUM,
+											STOCK_MAXIMIUM = :MAXIMIUM,
+											STOCK_OPEN_VALUE  = :OPENING_VALUE,
+											STOCK_CLOSE_VALUE = :CLOSE_VALUE
+											TB_COMPANY_COMPANY_PK = :COMPANY WHERE STOCK_PK = :PK');
+
+		$r = $q->execute(array(':MINIMIUM' => $this->minimium,
+							   ':MAXIMIUM' => $this->maximium,
+							   ':OPEN_VALUE' => $this->open_value,
+							   'CLOSE_VALUE' => $this->close_value,
+							   'COMPANY' => $this->company));
+
+		return $r;
 	}
 
-	public function getValue() {
-		return $this->value;
-	}
-	public function setValue($value) {
-		$this->value = $value;
-	}
+	public function delete() : bool {
+		$r = 0;
 
-	public function getClose_value() {
-		return $this->close_value;
-	}
-	public function setClose_value($close_value) {
-		$this->close_value = $close_value;
-	}
+		if (isset($this->pk)) {
+			$q = new Query('DELETE FROM TABLE TB_STOCK WHERE STOCK_PK = :PK');
+			$r = $q->execute(array('PK' => $this->pk));
 
-	public function getCompany() {
-		return $this->company;
+			if ($r) {
+				$this->pk = "";
+				$this->minimium = "";
+				$this->maximium = "";
+				$this->opening_value = "";
+				$this->close_value = "";
+				$this->company = "";
+			}
+		}
+
+		return $r;
 	}
-	public function setCompany($company) {
-		$this->company = $company;
-	}
+	
 }
